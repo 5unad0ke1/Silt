286acdc8720bee863ed1de2bcce5fbf7fa311aea
Oikawa <yucchi0424@gmail.com>
2025-10-30 16:00:16 +0900

    [add] ProjectTypeで使用したObjectPoolを追加

---
+++ Silt/Assets/Silt/Runtime/ObjPool/IMonoPool.cs
@@ -0,0 +1,11 @@
+﻿using Core;
+using UnityEngine;
+
+/// <summary>
+/// MonoPoolManagerを指定できるように実装する
+/// </summary>
+/// <typeparam name="TType">オブジェクトプールする型</typeparam>
+public interface IMonoPool<TType> where TType : Behaviour
+{
+    public void SetMonoPoolManager(MonoObjectPool<TType> manager);
+}
