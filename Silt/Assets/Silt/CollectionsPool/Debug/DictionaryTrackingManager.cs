cb924f4dfbdc8485085ad09a2f3107b1c41b7e5d
Oikawa <yucchi0424@gmail.com>
2026-01-31 14:04:34 +0900

    [update] lock実装。

--- Silt/Assets/Silt/CollectionsPool/Debug/DictionaryTrackingManager.cs
+++ Silt/Assets/Silt/CollectionsPool/Debug/DictionaryTrackingManager.cs
@@ -8,13 +8,16 @@ namespace Silt.CollectionsPool.Debug
         public static IReadOnlyCollection<DictionaryTrackingInfo> Collections => _collections;
         internal static void Register<TKey, TValue>(Func<int> getFreeCount, Func<int> getBusyCount)
         {
-            _collections.Add(new DictionaryTrackingInfo
+            lock (_lock)
             {
-                GetFreeCount = getFreeCount,
-                GetBusyCount = getBusyCount,
-                KeyType = typeof(TKey),
-                ValueType = typeof(TValue)
-            });
+                _collections.Add(new DictionaryTrackingInfo
+                {
+                    GetFreeCount = getFreeCount,
+                    GetBusyCount = getBusyCount,
+                    KeyType = typeof(TKey),
+                    ValueType = typeof(TValue)
+                });
+            }
         }
         private static readonly object _lock = new();
         private static readonly List<DictionaryTrackingInfo> _collections = new();
